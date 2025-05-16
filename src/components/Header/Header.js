0494f7f233de306b4861208434084afeddad6948
Abhi18704 <abhigk2004@gmail.com>
2025-05-16 10:28:30 +0530

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Nav, Logo, NavLink, Bars, NavMenu, NavBtn } from "./HeaderElements";
+
+const Header = ({ toggle }) => {
+  return (
+    <div className="Container" style={{padding: 0}}>
+      <Nav>
+        <Logo to="/">
+          <img
+            src="/a.png"
+            alt="A"
+            className="w-20 h-20 rounded-full object-cover"
+          />
+        </Logo>
+        <NavMenu>
+          <NavLink className="menu-item" to="projects">
+            Projects
+          </NavLink>
+          <NavLink className="menu-item" to="about">
+            About
+          </NavLink>
+          <NavLink className="menu-item" to="contact">
+            Contact
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <a
+            className="btn PrimaryBtn"
+            href="https://drive.google.com/file/d/16wgWZWh2fZog6CjacQu4PFgI-D06Bbap/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Resume
+          </a>
+        </NavBtn>
+        <Bars onClick={toggle} />
+      </Nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,220 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+   {
+    img: "/AI.jpg",
+    title: "AI Mock Interview",
+    description:
+      <ul>
+      <li>An AI-powered platform that generates personalized interview questions, conducts mock interviews, and saves them with unique interview links </li>
+      <li>Developed a fully functional web application on AI Mock Interview in 4 weeks.</li>
+      </ul>,
+    tech_stack: ["React.js","TAILWINDCSS","NODEJS"],
+    github_url: "https://github.com/Abhi18704/smart-interview-assistant",
+    demo_url: "https://smart-interview-assistant-eight.vercel.app/",
+  },
+  {
+    img: "/amazon.jpg",
+    title: "Amazon Clone",
+    description:
+      "Developed a Amazon clone in a period of 2 weeks",
+    tech_stack: ["HTML","CSS"],
+    github_url: "https://github.com/Abhi18704/Amazon-clone",
+    demo_url: "https://abhi18704.github.io/Amazon-clone/",
+  },
+  {
+    img: "/currency.jpg",
+    title: "Currency converter app",
+    description:
+      <ul>
+        <li> Developed a fully functional web application on currency converter in 3 weeks.</li>
+        <li> Accurate and Up-to-Date Exchange Rates.</li>
+        <li>User-Friendly Interface</li>
+        <li> Support for Multiple Currencies </li>
+      </ul>,
+    tech_stack: ["HTML","CSS","JS"],
+    github_url: "https://github.com/Abhi18704/Currency-Converter",
+    demo_url: "https://abhi18704.github.io/Currency-Converter/",
+  },
+  {
+    img: "/movie-review.jpg",
+    title: "Movie review app",
+    description:
+      <ul>
+        <li>We can explore the latest movie reviews and rating </li>
+      </ul>,
+    tech_stack: ["HTML","CSS","JS"],
+    github_url: "https://github.com/Abhi18704/movie-review-app",
+    demo_url:"",
+  },
+  {
+    img: "/GitHub.jpg",
+    title: "Github profile search",
+    description:
+      <ul>
+        <li>Implemented a responsive GitHub profile search application with real-time updates</li>
+        <li>Advanced Filtering and Customization</li>
+        <li> Real-Time Data Integration </li>
+      </ul>,
+    tech_stack: ["HTML","CSS","JS"],
+    github_url: "https://github.com/Abhi18704/GitHub-profile-search",
+    demo_url:"https://abhi18704.github.io/GitHub-profile-search/",
+  },
+  {
+    img: "/toursim.png",
+    title: "Toursim Management system",
+    description:
+      <ul>
+        <li> Displays a list of available tour packages with details such as destination, duration, and price.</li>
+        <li> Allows users to select and book tour packages</li>
+        <li>  Facilitates booking of tour packages with details like the number of travelers, travel dates, and package selection. </li>
+        <li> Stores and manages customer details, including name, contact information, and booking history.</li>
+      </ul>,
+    tech_stack: ["C"],
+    github_url: "https://github.com/Abhi18704/Tourism-management-system",
+    demo_url:"",
+  },
+];
+  /*
+  {
+    img: "/renr-responsive.png",
+    title: "RENR Safety App",
+    description:
+      "A safety application for the Renewable Resources Dept. of BCIT",
+    tech_stack: ["React", "Firebase"],
+    github_url: "",
+    demo_url: "https://renr-bcit.web.app/",
+  },
+  {
+    img: "/mymind-responsive.png",
+    title: "MyMind",
+    description:
+      "An app where users can purchase online therapy sessions and chat with their therapists.",
+    tech_stack: ["HTML", "CSS", "NodeJs", "MongoDB", "SocketIo"],
+    github_url: "https://github.com/salesp07/MyMind---Copy",
+    demo_url: "https://mymindweb.vercel.app/",
+  },
+  {
+    img: "/pokedex-responsive.png",
+    title: "Pokedex",
+    description:
+      "A pokedex app that displays pokemon data, plays OSTs and makes pokemons dance.",
+    tech_stack: ["ReactJs", "NodeJs", "MongoDB"],
+    github_url: "https://github.com/salesp07/Pokedex-ReactApp",
+    demo_url: "https://pokedex-salesp07.netlify.app/",
+  },
+  {
+    img: "/jobDetect.png",
+    title: "JobDetect",
+    description:
+      "An app to detect if a job posting is a scam or not using its description.",
+    tech_stack: ["Python", "Flask", "NodeJs", "MongoDB"],
+    github_url: "https://github.com/salesp07/JobDetectNode",
+    demo_url: "https://jobdetective.onrender.com/",
+  },
+];*/
+
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  /*
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+    name: "NextJs",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    name: "Typescript",
+  },*/
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  /*
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "React Native",
+  },*/
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/oracle/oracle-original.svg",
+    name: "SQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/oracle/oracle-original.svg",
+    name: "PLSQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  /*
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original.svg",
+    name: "Flask",
+  },*/
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+    name: "Java",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/angularjs/angularjs-original.svg",
+    name:"AnjularJs"
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg",
+    name:"GitHub"
+  },
+  /*
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+    name: "Material UI",
+  },*/
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+    name: "Figma",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-original.svg",
+    name: "TailwindCSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
